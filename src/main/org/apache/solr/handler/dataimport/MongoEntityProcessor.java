12db7eefe3343aef6640bd932e2b63ac12791efe
gem update <marciogoda@gmail.com>
2015-07-09 15:58:20 +0100

    Added support to Delta Import

--- src/main/org/apache/solr/handler/dataimport/MongoEntityProcessor.java
+++ src/main/org/apache/solr/handler/dataimport/MongoEntityProcessor.java
@@ -3,7 +3,6 @@ package org.apache.solr.handler.dataimport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Iterator;
 import java.util.Map;
 
 import static org.apache.solr.handler.dataimport.DataImportHandlerException.SEVERE;
@@ -49,13 +48,72 @@ public class MongoEntityProcessor extends EntityProcessorBase {
 
     @Override
     public Map<String, Object> nextRow() {
-        String query = context.getEntityAttribute( QUERY );
         if (rowIterator == null) {
+            String query = this.getQuery();
             initQuery(context.replaceTokens(query));
+
         }
         return getNext();
     }
 
+    @Override
+    public Map<String, Object> nextModifiedRowKey() {
+        if (rowIterator == null) {
+            String deltaQuery = context.getEntityAttribute(DELTA_QUERY);
+            if(deltaQuery == null)
+                return null;
+            initQuery(context.replaceTokens(deltaQuery));
+
+        }
+        return getNext();
+    }
+
+    @Override
+    public Map<String, Object> nextDeletedRowKey() {
+        if (rowIterator == null) {
+            String deletedPkQuery = context.getEntityAttribute(DEL_PK_QUERY);
+            if(deletedPkQuery == null)
+                return null;
+            initQuery(context.replaceTokens(deletedPkQuery));
+        }
+        return getNext();
+    }
+
+    @Override
+    public Map<String, Object> nextModifiedParentRowKey() {
+        if(this.rowIterator == null) {
+            String parentDeltaQuery = this.context.getEntityAttribute("parentDeltaQuery");
+            if(parentDeltaQuery == null) {
+                return null;
+            }
+
+            LOG.info("Running parentDeltaQuery for Entity: " + this.context.getEntityAttribute("name"));
+            this.initQuery(this.context.replaceTokens(parentDeltaQuery));
+        }
+
+        return this.getNext();
+    }
+
+    public String getQuery() {
+        String queryString = this.context.getEntityAttribute(QUERY);
+        if("FULL_DUMP".equals(this.context.currentProcess())) {
+            return queryString;
+        } else if("DELTA_DUMP".equals(this.context.currentProcess())) {
+            return this.context.getEntityAttribute(DELTA_IMPORT_QUERY);
+
+        } else {
+            return null;
+        }
+    }
+
+
     public static final String QUERY      = "query";
+
+    public static final String DELTA_QUERY = "deltaQuery";
+
+    public static final String DELTA_IMPORT_QUERY = "deltaImportQuery";
+
+    public static final String DEL_PK_QUERY = "deletedPkQuery";
+
     public static final String COLLECTION = "collection";
 }
